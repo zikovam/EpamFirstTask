1b754564396e41add22c2a0afe6e2ad1f4ffbeb2
Alexandr Zykov <zikovam43@gmail.com>
2016-03-25 15:50:45 +0300

    Поправил код на основе автоматических рекомендаций

---
+++ src/main/java/testForMe.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zikovam on 12.03.16.
+ */
+import ru.spb.epam.azykov.first.FirstTask;
+
+public class testForMe {
+
+    public static void main( String[] args ) {
+        FirstTask tasks = new FirstTask();
+
+        tasks.task25();
+    }
+}
